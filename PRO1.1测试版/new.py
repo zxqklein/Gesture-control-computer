db7af5bf7787ac1d6612e54dd42108112f7c8176
Mr.Aphe <105808215+baalemonkland@users.noreply.github.com>
2023-02-24 19:51:45 +0800

    Add files via upload

---
+++ PRO1.1测试版/new.py
@@ -0,0 +1,131 @@
+import math
+def vector_2d_angle(v1,v2):
+    """
+        求解二维向量的角度
+         角度公式angle= arccos((v1_x*v2_x + v1_y*v2_y) / (|v1|*|v2|))
+         计算两个二维向量v1和v2之间的夹角，并将其以角度的形式赋值给angle_变量，如果角度大于180度，则将angle_设置为65535.
+    """
+    v1_x = v1[0]
+    v1_y = v1[1]
+    v2_x = v2[0]
+    v2_y = v2[1]
+
+    try:
+        angle_12= math.degrees(math.acos((v1_x*v2_x+v1_y*v2_y)/(((v1_x**2+v1_y**2)**0.5)*((v2_x**2+v2_y**2)**0.5))))
+    except ZeroDivisionError:
+        angle_12 =65535.
+    if angle_12 > 180.:
+        angle_12 = 65535.
+    return angle_12
+
+
+def hand_angle(hand_):
+    """
+        获取对应手相关向量的二维角度,根据角度确定手势
+    """
+    angle_list = []
+    # ---------------------------- thumb 大拇指角度
+    angle_ = vector_2d_angle(
+        ((int(hand_[0][0])- int(hand_[2][0])),(int(hand_[0][1])-int(hand_[2][1]))),
+        ((int(hand_[3][0])- int(hand_[4][0])),(int(hand_[3][1])- int(hand_[4][1])))
+        )
+    angle_list.append(angle_)
+    # ---------------------------- index 食指角度
+    angle_ = vector_2d_angle(
+        ((int(hand_[0][0])-int(hand_[6][0])),(int(hand_[0][1])- int(hand_[6][1]))),
+        ((int(hand_[7][0])- int(hand_[8][0])),(int(hand_[7][1])- int(hand_[8][1])))
+        )
+    angle_list.append(angle_)
+    # ---------------------------- middle 中指角度
+    angle_ = vector_2d_angle(
+        ((int(hand_[0][0])- int(hand_[10][0])),(int(hand_[0][1])- int(hand_[10][1]))),
+        ((int(hand_[11][0])- int(hand_[12][0])),(int(hand_[11][1])- int(hand_[12][1])))
+        )
+    angle_list.append(angle_)
+    # ---------------------------- ring 无名指角度
+    angle_ = vector_2d_angle(
+        ((int(hand_[0][0])- int(hand_[14][0])),(int(hand_[0][1])- int(hand_[14][1]))),
+        ((int(hand_[15][0])- int(hand_[16][0])),(int(hand_[15][1])- int(hand_[16][1])))
+        )
+    angle_list.append(angle_)
+    # ---------------------------- pink 小拇指角度
+    angle_ = vector_2d_angle(
+        ((int(hand_[0][0])- int(hand_[18][0])),(int(hand_[0][1])- int(hand_[18][1]))),
+        ((int(hand_[19][0])- int(hand_[20][0])),(int(hand_[19][1])- int(hand_[20][1])))
+        )
+    angle_list.append(angle_)
+    return angle_list
+
+
+def h_gesture(angle_list):
+    """
+        # 二维约束的方法定义手势
+        # fist five gun love one six three thumbup yeah
+    """
+    thr_angle = 65.  # 手指闭合则大于这个值（大拇指除外）
+    thr_angle_thumb = 53.  # 大拇指闭合则大于这个值
+    thr_angle_s = 49.  # 手指张开则小于这个值
+    gesture_str = "Unknown"
+    g_id = 8080
+    if 65535. not in angle_list:
+        if (angle_list[0] > thr_angle_thumb) and (angle_list[1] > thr_angle) and (angle_list[2] > thr_angle) and (
+                angle_list[3] > thr_angle) and (angle_list[4] > thr_angle):
+            gesture_str = "0"
+            g_id = 0
+        elif (angle_list[0] > thr_angle_thumb) and (angle_list[1] < thr_angle_s) and (angle_list[2] > thr_angle) and (
+                angle_list[3] > thr_angle) and (angle_list[4] > thr_angle):
+            gesture_str = "1"
+            g_id = 1
+        elif (angle_list[0] > thr_angle_thumb) and (angle_list[1] < thr_angle_s) and (angle_list[2] < thr_angle_s) and (
+                angle_list[3] > thr_angle) and (angle_list[4] > thr_angle):
+            gesture_str = "2"
+            g_id = 2
+        elif (angle_list[0] > thr_angle_thumb) and (angle_list[1] < thr_angle_s) and (angle_list[2] < thr_angle_s) and (
+                angle_list[3] < thr_angle_s) and (angle_list[4] > thr_angle):
+            gesture_str = "3"
+            g_id = 3
+        elif (angle_list[0] > thr_angle_thumb) and (angle_list[1] < thr_angle_s) and (angle_list[2] < thr_angle_s) and (
+                angle_list[3] < thr_angle_s) and (angle_list[4] < thr_angle_s):
+            gesture_str = "4"
+            g_id = 4
+        elif (angle_list[0] < thr_angle_s) and (angle_list[1] < thr_angle_s) and (angle_list[2] < thr_angle_s) and (
+                angle_list[3] < thr_angle_s) and (angle_list[4] < thr_angle_s):
+            gesture_str = "5"
+            g_id = 5
+        elif (angle_list[0] < thr_angle_s) and (angle_list[1] > thr_angle) and (angle_list[2] > thr_angle) and (
+                angle_list[3] > thr_angle) and (angle_list[4] < thr_angle_s):
+            gesture_str = "6"
+            g_id = 6
+        elif (angle_list[0] < thr_angle_s) and (angle_list[1] < thr_angle_s) and (angle_list[2] > thr_angle) and (
+                angle_list[3] > thr_angle) and (angle_list[4] > thr_angle):
+            gesture_str = "8"
+            g_id = 8
+        elif (angle_list[0] > thr_angle_thumb) and (angle_list[1] > thr_angle) and (angle_list[2] > thr_angle) and (
+                angle_list[3] > thr_angle) and (angle_list[4] < thr_angle_s):
+            gesture_str = "Pink Up"
+            g_id = 10
+        elif (angle_list[0] < thr_angle_s) and (angle_list[1] > thr_angle) and (angle_list[2] > thr_angle) and (
+                angle_list[3] > thr_angle) and (angle_list[4] > thr_angle):
+            gesture_str = "Thumb Up"
+            g_id = 11
+        elif (angle_list[0] > thr_angle_thumb) and (angle_list[1] > thr_angle) and (angle_list[2] < thr_angle_s) and (
+                angle_list[3] > thr_angle) and (angle_list[4] > thr_angle):
+            gesture_str = "Fuck"
+            g_id = 414
+        elif (angle_list[0] > thr_angle_thumb) and (angle_list[1] > thr_angle) and (angle_list[2] < thr_angle_s) and (
+                angle_list[3] < thr_angle_s) and (angle_list[4] < thr_angle_s):
+            gesture_str = "Princess"
+            g_id = 666
+        elif (angle_list[0] < thr_angle_s) and (angle_list[1] < thr_angle_s) and (angle_list[2] < thr_angle_s) and (
+                angle_list[3] > thr_angle) and (angle_list[4] > thr_angle):
+            gesture_str = "Bye"
+            g_id = 886
+        elif (angle_list[0] < thr_angle_s) and (angle_list[1] < thr_angle_s) and (angle_list[2] > thr_angle) and (
+                angle_list[3] > thr_angle) and (angle_list[4] < thr_angle_s):
+            gesture_str = "Spider-Man"
+            g_id = 555
+        elif (angle_list[0] > thr_angle_thumb) and (angle_list[1] < thr_angle_s) and (angle_list[2] > thr_angle) and (
+                angle_list[3] > thr_angle) and (angle_list[4] < thr_angle_s):
+            gesture_str = "Rock'n'Roll"
+            g_id = 999
+    return (gesture_str, g_id)
